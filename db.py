caa18855dabbbcd32d6543b91d347c67cc30acc8
SANTHOSH KUMAR SADHU <santhoshkumarsadu2000@gmail.com>
2026-07-14 13:42:58 +0530

    latest updates

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
+import os
 import pymysql
 
 def get_connection():
     connection = pymysql.connect(
-        host="localhost",
-        user="root",
-        password="Santhosh@1982",
-        database="student_result_db"
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        user=os.environ.get("root"),
+        password=os.environ.get("Santhosh@1982"),
+        database=os.environ.get("student_result_db"),
     )
     return connection
