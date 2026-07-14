f09faf68852cddcb318d57fdb7552372320b0984
SANTHOSH KUMAR SADHU <santhoshkumarsadu2000@gmail.com>
2026-07-14 14:55:09 +0530

    latest update0s

--- db.py
+++ db.py
@@ -3,8 +3,10 @@ import pymysql
 def get_connection():
     connection = pymysql.connect(
         host="mysql-3573fd4b-santhoshkumarsadu2000-e072.a.aivencloud.com",
+        port=12864,
         user="avnadmin",
         password="AVNS_1h9ZJi7XgqcijG_64Qu",
-        database="defaultdb"
+        database="defaultdb",
+        ssl={'ca': 'C:\Users\dhill\Downloads\santhoshkumarsadhu\Student-Result-Analytics-System\ca.pem'}
     )
     return connection
