b0d4e505ebc867ffa435a17843035ca0e37a93da
SANTHOSH KUMAR SADHU <santhoshkumarsadu2000@gmail.com>
2026-07-14 14:07:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,10 @@
-import os
 import pymysql
 
 def get_connection():
     connection = pymysql.connect(
-        host=os.environ.get("DB_HOST"),
-        port=int(os.environ.get("DB_PORT", 3306)),
-        user=os.environ.get("root"),
-        password=os.environ.get("Santhosh@1982"),
-        database=os.environ.get("student_result_db"),
+        host="mysql-3573fd4b-santhoshkumarsadu2000-e072.a.aivencloud.com",
+        user="avnadmin",
+        password="AVNS_1h9ZJi7XgqcijG_64Qu",
+        database="defaultdb"
     )
     return connection
